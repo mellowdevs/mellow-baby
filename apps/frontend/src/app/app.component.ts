6b45b749e01c86a565705c6b0ca7837d29801f27
Melike Kubik <kavcioglumelike@gmail.com>
2025-06-14 23:15:30 +0200

    feat(sidebar): sidebar skeleton created and theme/language selector buttons are moved into

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -6,38 +6,20 @@ import { ThemeService } from './theme/theme.service';
 import { CommonModule } from '@angular/common';
 import { TranslocoService } from '@ngneat/transloco';
 import { FormsModule } from '@angular/forms';
+import { SidebarComponent } from './shared/components/layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule, FormsModule, DropdownModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ButtonModule,
+    FormsModule,
+    DropdownModule,
+    SidebarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  // inject() is a modern and clean way to get services
-  themeService = inject(ThemeService);
-  translocoService = inject(TranslocoService);
-
-  languages = [
-    { name: 'English', code: 'en', flag: 'gb' },
-    { name: 'Türkçe', code: 'tr', flag: 'tr' },
-    { name: 'Deutsch', code: 'de', flag: 'de' },
-  ];
-
-  /**
-   * changes between light and dark mode
-   */
-  toggleDarkMode() {
-    this.themeService.toggleTheme();
-  }
-
-  /**
-   * Changes the application's active language.
-   * @param lang The language code to switch to (e.g., 'en', 'tr', 'de').
-   */
-  changeLang(lang: string) {
-    this.translocoService.setActiveLang(lang);
-    localStorage.setItem('lang', lang);
-  }
-}
+export class AppComponent {}
--- apps/frontend/src/app/shared/components/layout/sidebar/sidebar.component.ts
+++ apps/frontend/src/app/shared/components/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { TranslocoModule, TranslocoService } from '@ngneat/transloco';
+import { RouterLink } from '@angular/router';
+import { ThemeService } from '../../../../theme/theme.service';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { MenuItem } from 'primeng/api';
 
+interface Langugage {
+  name: string;
+  code: string;
+  flag: string;
+}
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    DropdownModule,
+    TranslocoModule,
+    RouterLink,
+    SplitButtonModule,
+    SpeedDialModule,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  themeService = inject(ThemeService);
+  public translocoService = inject(TranslocoService);
+  public selectedLanguage: any = localStorage.getItem('lang') || 'en';
+  languages: Langugage[] = [
+    { name: 'English', code: 'en', flag: 'gb' },
+    { name: 'Türkçe', code: 'tr', flag: 'tr' },
+    { name: 'Deutsch', code: 'de', flag: 'de' },
+  ];
+  languageMenuItems: MenuItem[] = [];
+  currentLanguage: Langugage =
+    this.languages.find((lang) => lang.code === localStorage.getItem('lang')) || this.languages[0];
 
+  ngOnInit() {
+    this.translocoService.langChanges$.subscribe((activeLang) => {
+      this.setCurrentLangugage(activeLang);
+      this.buildLanguageMenuItems();
+    });
+  }
+
+  toggleDarkMode() {
+    this.themeService.toggleTheme();
+  }
+
+  setCurrentLangugage(langCode: string) {
+    this.currentLanguage = this.languages.find((l) => l.code === langCode) || this.languages[0];
+    localStorage.setItem('lang', langCode);
+  }
+
+  private buildLanguageMenuItems(): void {
+    // Build the menu items array, excluding the currently active language
+    this.languageMenuItems = this.languages
+      .filter((l) => l.code !== this.currentLanguage.code)
+      .map((l) => ({
+        label: l.name,
+        icon: `fi fi-${l.flag}`,
+        command: () => this.changeLang(l.code),
+      }));
+  }
+
+  changeLang(langCode: string): void {
+    this.translocoService.setActiveLang(langCode);
+  }
 }
