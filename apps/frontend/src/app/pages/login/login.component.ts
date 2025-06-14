647eadc2308a8ca37fc8849c26e4d61c93d073ca
Melike Kubik <kavcioglumelike@gmail.com>
2025-06-14 15:19:14 +0200

    feat(login): connect loign to MellowAPI

--- apps/frontend/src/app/pages/login/login.component.ts
+++ apps/frontend/src/app/pages/login/login.component.ts
@@ -3,41 +3,54 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
-import { FloatLabelModule } from 'primeng/floatlabel';
+import { DynamicFormComponent } from '../../shared/components/dynamic-form/dynamic-form.component';
+import { FormItem } from '../../shared/models/form-item.model';
+import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    ButtonModule,
-    InputTextModule,
-    CardModule,
-    FloatLabelModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, ButtonModule, CardModule, DynamicFormComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
 
   loginForm: FormGroup;
+  loginFormItems: FormItem[];
 
   constructor() {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
     });
+
+    this.loginFormItems = [
+      {
+        controlName: 'email',
+        label: 'Email',
+        type: 'email',
+        id: 'login-email',
+      },
+      {
+        controlName: 'password',
+        label: 'Password',
+        type: 'password',
+        id: 'login-password',
+      },
+    ];
   }
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      console.log('Form Submitted!', this.loginForm.value);
-    } else {
-      console.log('Form is invalid');
-    }
+    if (this.loginForm.invalid) return;
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (resp) => {
+        console.log('success');
+      },
+      error: (err) => console.error(err),
+    });
   }
 }
