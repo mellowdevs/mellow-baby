971aa6e55b5726d76e1600a2ec6f6772afc26f98
Melike Kubik <kavcioglumelike@gmail.com>
2025-06-16 20:31:35 +0200

    feat(message): toast message is added

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
-import { ThemeService } from './theme/theme.service';
+import { ToastModule } from 'primeng/toast';
 import { CommonModule } from '@angular/common';
-import { TranslocoService } from '@ngneat/transloco';
 import { FormsModule } from '@angular/forms';
 import { SidebarComponent } from './shared/components/layout/sidebar/sidebar.component';
 
@@ -18,6 +17,7 @@ import { SidebarComponent } from './shared/components/layout/sidebar/sidebar.com
     FormsModule,
     DropdownModule,
     SidebarComponent,
+    ToastModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
