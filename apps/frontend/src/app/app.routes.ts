e1405f0a96ce68569a297db4abed15eb10990248
Melike Kubik <kavcioglumelike@gmail.com>
2025-06-17 20:51:59 +0200

    feat: not found and app logo added

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -5,13 +5,23 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './core/guards/auth.guard';
 import { loginRedirectGuard } from './auth/login-redirect.guard';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MainLayoutComponent } from './shared/components/layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [loginRedirectGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [loginRedirectGuard] },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  // Add a default route to redirect to the dashboard
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  // Add a wildcard route for any other path
-  { path: '**', component: NotFoundComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+
+      // Add a default route to redirect to the dashboard
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      // Wildcard route FOR LOGGED-IN USERS. Must be the last child.
+      // This will show the NotFoundComponent inside the main layout with the sidebar.
+      { path: '**', component: NotFoundComponent },
+    ],
+  },
 ];
